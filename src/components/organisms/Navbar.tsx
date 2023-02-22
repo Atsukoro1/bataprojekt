a0b97d1d2ca5311e14fe814b2ea1aad85dbd87e8
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-22 18:43:14 +0100

    feat: 🎸 Init of other pages

---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,13 @@
+export default () => {
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-gray-900 pr-9 pl-9">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <img 
+                    className="h-[80px] w-[80px] mr-2" 
+                    src="https://media.discordapp.net/attachments/1077856507867172894/1077857330621853757/IMG_2807.png?width=606&height=606" 
+                    alt="Logo"
+                />
+            </div>
+        </nav>
+    )
+}
