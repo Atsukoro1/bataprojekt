a6af7dcf065498ea915d055f5cc07d9a46c31a19
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-23 09:26:54 +0100

    feat: 🎸 Logo]

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -6,7 +6,11 @@ const Navbar = () => {
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <Image 
                     className="h-[80px] w-[80px] mr-2" 
-                    src="https://media.discordapp.net/attachments/1077856507867172894/1077857330621853757/IMG_2807.png?width=606&height=606" 
+                    src={{
+                        src: "/logo.png",
+                        height: 50,
+                        width: 50
+                    }}
                     alt="Logo"
                 />
             </div>
