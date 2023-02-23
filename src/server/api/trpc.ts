f3e3833ce13641076a1dc9d4c13be8693de0cb6f
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-23 09:35:17 +0100

    feat: 🎸 Remove comment

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,19 +1,3 @@
-/**
- * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
- * 1. You want to modify request context (see Part 1).
- * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
- */
-
-/**
- * 1. CONTEXT
- *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
- */
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
 
@@ -24,16 +8,6 @@ type CreateContextOptions = {
   session: Session | null;
 };
 
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
- */
 const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
