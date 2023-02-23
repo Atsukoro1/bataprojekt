8669eaba2b192b19b19e71f074ab02c5369272b5
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-23 12:17:23 +0100

    feat: 🎸 Changes

--- src/pages/game/1.tsx
+++ src/pages/game/1.tsx
@@ -16,24 +16,24 @@ const Section1 = () => {
     };
 
     const content = useMemo(() => {
-        switch(progress) {
+        switch (progress) {
             case 0:
                 return (
                     <ContentVideo
-                    videoSrc="https://www.youtube.com/watch?v=1m92LSrvi3k"
-                    onInnerClose={() => setProgress(progress + 1)}
-                />
+                        videoSrc="https://www.youtube.com/watch?v=1m92LSrvi3k"
+                        onInnerClose={() => setProgress(progress + 1)}
+                    />
                 )
-    
+
             case 1:
                 return (
-                    <ContentImage 
+                    <ContentImage
                         onInnerClose={() => setProgress(progress + 1)}
                         imageSrc="https://cdn.myshoptet.com/usr/www.kartografie.cz/user/shop/big/2367-4_2367-svet-nastenna-obecne-zemepisna-mapa.jpg?621e11d2"
                     />
                 )
-    
-            default: 
+
+            default:
                 return (
                     <ContentFinal
                         title="Gratulujeme!"
@@ -47,8 +47,12 @@ const Section1 = () => {
     return (
         <main className="flex min-h-screen flex-col items-center bg-slate-800">
             <div className="mt-20">
-                <h1 className="text-white text-lg mb-3">Stanoviště 1</h1>
-                
+                <h1 className="text-white text-lg mb-3">1 LOKACE</h1>
+                <p>
+                    Výborně, úspěšně jsi našel první lokaci!
+                    Právě se nacházíš na začátku našeho případu, který dostal na starost detektiv Jonáš Červenka.
+                </p>
+
                 {content}
             </div>
         </main>
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -16,23 +16,32 @@ const IndexPage = () => {
 
     const gameSession = api.gameSession.fetchSession.useQuery(undefined, {
         onSuccess: (data) => {
-            (async function() {
+            (async function () {
                 await Router.push(`/game/${data?.stage.toString()}`);
             }());
         }
     });
 
     const onStartSession = async () => {
-        if(gameSession.data) {
+        if (gameSession.data) {
             toast.error("Již máte vytvořenou hru");
         } else {
             await createGameSession.mutateAsync();
         }
     }
-    
+
     return (
         <main className="flex min-h-screen flex-col items-center bg-slate-800">
             <div className="mt-20">
+                <h1>Vítáme tě!</h1>
+                <p>
+                    Právě jsi dostal možnost zahrát si únikovou hru.
+                    Tato hra byla vytvořena studenty 3. ročníku multimediální školy, ve Zlíně.
+                    <br /><br />
+                    Jestli si chceš zahrát rychlou detektivku a uniknout vrahovi, klikni na tlačítko START, poté dostaneš první indicie a přesměrujeme tě na první lokaci.
+                    <br /><br />
+                    (po zmáčknutí tlačítka START se ukáže první obrázek mapy)
+                </p>
                 {
                     gameSession.isLoading ? (
                         <Image
@@ -44,7 +53,7 @@ const IndexPage = () => {
                             alt='loading'
                         />
                     ) : (
-                        <button 
+                        <button
                             className="bg-slate-600 text-white p-4 rounded-lg"
                             onClick={onStartSession}
                         >
