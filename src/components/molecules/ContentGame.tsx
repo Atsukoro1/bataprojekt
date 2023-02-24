b0afcd6f5c6fadf85a8fbb4a5f09c5fc4ac0604f
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-24 08:28:05 +0100

    feat: 🎸 Done button'

--- src/components/molecules/ContentGame.tsx
+++ src/components/molecules/ContentGame.tsx
@@ -22,22 +22,12 @@ const ContentGame = ({
     onSuccess,
     onFail
 }: ContentGameProps) => {
-    const [inputCode, setInputCode] = useState<string>("");
-
     const { unityProvider } = useUnityContext({
         loaderUrl: loaderUrl,
         dataUrl: dataUrl,
         frameworkUrl: frameworkUrl,
         codeUrl: codeUrl
     });
-
-    const onCodeSubmit = () => {
-        if (code.localeCompare(inputCode) === 0) {
-            onSuccess();
-        } else {
-            onFail();
-        }
-    }
 
     return (
         <div>
@@ -48,33 +38,11 @@ const ContentGame = ({
                 unityProvider={unityProvider}
             />
 
-            <input
-                type="text"
-                placeholder="Kod ze hry"
-                className="border-slate-300 bg-slate-600 rounded-lg p-2"
-                onKeyDown={(event) => {
-                    const key = event.key;
-                    const validChar = /[a-zA-Z0-9]/;
-                    const isBackspace = key === "Backspace";
-
-                    if (isBackspace) {
-                        setInputCode(prevState => prevState.slice(0, -1));
-                    } else {
-                        if (validChar.test(key) && key.length === 1) {
-                            setInputCode(prevState => prevState + key);
-                        } else if (key === " ") {
-                            setInputCode(prevState => `${prevState} `);
-                        };
-                    }
-                }}
-                value={inputCode}
-            />
-
             <button
                 className="bg-slate-400 p-3 rounded-lg text-white mt-3"
-                onClick={onCodeSubmit}
+                onClick={onSuccess}
             >
-                Odeslat kod
+                Hotovo
             </button>
         </div>
     )
